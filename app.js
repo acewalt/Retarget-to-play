94efd03204f8f32530bb987ce8fd707d92eaa741
walt_to_art <guard.mauricio.save@gmail.com>
2026-09-19 21:01:01 -0500

    Export original-rig action sidecar and restore preview

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { FBXExporter } from '@comfyorg/fbx-exporter-three';
 import { WaltFBXLoader, WALT_FBX_VERSION } from './walt-fbx-loader.js?v=20260920-rt2';
-import { injectAnimationsIntoOriginalFBX } from './walt-fbx-exact-export.js?v=20260920-action1';
-import { buildBlenderActionScript } from './blender-action-export.js?v=20260920-action2';
+import { injectAnimationsIntoOriginalFBX } from './walt-fbx-exact-export.js?v=20260920-action3';
+import { buildBlenderActionScript } from './blender-action-export.js?v=20260920-action3';
 
 const $ = (id) => document.getElementById(id);
 const fbxLoader = new WaltFBXLoader();
